dbeaf7bd6aa1188269f94c63eeac4ed88fb231c4
Daniel Lam <dlam@usc01-wzjgh8.8x8.com>
2019-10-10 11:23:33 -0700

    test commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     // ]
     "extends": [
       "react-app",
+      //allows prettier extension to format the files
       "plugin:prettier/recommended"
     ],
     "parser": "babel-eslint",
