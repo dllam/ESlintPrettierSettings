33f4ca38b068e73f8f9b419f88790a1d652e816b
Daniel Lam <dlam@usc01-wzjgh8.8x8.com>
2019-10-10 10:30:25 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    // extends: `react-app`,
+
+
+    // "parser": "babel-eslint",
+    // "rules": {
+    //   "strict": 0
+    // },
+    // "extends": [
+    //   "eslint:recommended",
+    //   "plugin:react/recommended"
+    // ]
+    "extends": [
+      "react-app",
+      "plugin:prettier/recommended"
+    ]
+  }
